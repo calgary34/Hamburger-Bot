842634986bf1ad4917782490a68ef58c46f0c548
Spot Sign <aaron34weston@gmail.com>
2021-01-06 18:26:15 +0000

    Dice command

--- bot.py
+++ bot.py
@@ -7,14 +7,20 @@ client = commands.Bot(command_prefix=";")
 client.remove_command("help")
 
 @client.event
+async def on_message(msg):
+  msg.content = msg.content.lower()
+  await client.process_commands(msg)
+
+@client.event
 async def on_ready():
+	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="For ;help"))
 	print(client.user, "Is Ready")
 
 @client.command(aliases=['random-integer'])
 async def random_integer( ctx,startnum:int,endnum:int):
-	answer=f'Your random integer between {str(startnum)} and {str(endnum)} is {str(random.randint(int(startnum),int(endnum)))}.'
-	em = discord.Embed(title="Random Integer", description=answer, color=0x275ef4)
-	await ctx.send(embed=em)
+  answer=f'Your random integer between {str(startnum)} and {str(endnum)} is {str(random.randint(int(startnum),int(endnum)))}.'
+  em = discord.Embed(title="Random Integer", description=answer, color=0x275ef4)
+  await ctx.send(embed=em)
 
 
 @client.command(aliases=['random-float'])
@@ -25,10 +31,10 @@ async def random_float(ctx,startnum:float,endnum:float):
 
 @client.command(aliases=['random-letter'])
 async def random_letter( ctx):
-	letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-	answer=f'{ctx.author.mention}, your random letter is `{random.choice(letters)}`.'
-	em = discord.Embed(title="Random Letter", description=answer, color=0x275ef4)
-	await ctx.send(embed=em)
+  letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  answer=f'{ctx.author.mention}, your random letter is `{random.choice(letters)}`.'
+  em = discord.Embed(title="Random Letter", description=answer, color=0x275ef4)
+  await ctx.send(embed=em)
 
 
 @client.command(aliases=['random-shuffle','shuffle'])
@@ -51,20 +57,21 @@ async def random_choice( ctx,*args):
 
 @client.command()
 async def info(ctx):
-  em = discord.Embed(title="Info about bot",description='Hamburger Bot is a Discord bot created by [Spot Sign](https:repl.it/@SpotSign) and [The Peeps191](https:repl.it/@ThePeeps191). The code is open-source and can be found [here](https://github.com/calgary34/Hamburger-Bot).', color=0x275ef4)
+  em = discord.Embed(title="Info about bot",description='Hamburger Bot is a Discord bot created by [Spot Sign](https://repl.it/@SpotSign) and [The Peeps191](https://repl.it/@ThePeeps191). The code is open-source and can be found [here](https://github.com/calgary34/Hamburger-Bot).', color=0x275ef4)
   await ctx.send(embed=em)
 
 @client.command(aliases=['8ball'])
 async def eightball( ctx,*,question):
   answers=["It is certain.","It is decidedly so.","Without a doubt.","Yes - definitely","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.",'Better not tell you now.','Cannot predict now.','Concentrate and ask again.',"Don't count on it.",'My reply is no.','My sources say no.','Outlook not so good.','Very doubtful.']
-  em = discord.Embed(title="8ball",description=f'You question: {question}\n8ball says: {random.choice(answers)}', color=0x275ef4)
+  em = discord.Embed(title="8ball",description=f'Your question: {question}\n8ball says: {random.choice(answers)}\n:8ball::8ball::8ball::8ball::8ball:', color=0x275ef4)
   await ctx.send(embed=em)
 
 @client.group(invoke_without_command=True)
 async def help(ctx):
-	em = discord.Embed(title="Help", description="Hamburger Bot - Help Command. You can use `;help <command>` for information on a certain command. `;info` also gives you information on the bot. Remember to use `;` before each command!", color=0x275ef4)
-	em.add_field(name="Random Commands", value="`random-integer`, `random-float`, `random-letter`, `random-shuffle`, `random-choice`, `8ball`")
-	await ctx.send(embed=em)
+  em = discord.Embed(title="Help", description="Hamburger Bot - Help Command. You can use `;help <command>` for information on a certain command. `;info` also gives you information on the bot. Remember to use `;` before each command!", color=0x275ef4)
+  em.add_field(name="Random Commands", value="`random-integer`, `random-float`, `random-letter`, `random-shuffle`, `random-choice`, `8ball`,`dice`")
+  await ctx.send(embed=em)
+
 class HelpCommand:
   def __init__(self, title, desc, usage,aliases):
     self.title = title
@@ -109,6 +116,34 @@ async def help_info(ctx):
 async def help_help(ctx):
   cmd=HelpCommand("Help on `help`","Help for the bot. You can also enter a command, giving help on that command.","`;help <command (optional)>`","None")
   await ctx.send(embed=cmd.em)
+
+@help.command(name='dice')
+async def help_dice(ctx):
+  cmd=HelpCommand("Help on `dice`","Rolls dice! You can specify the number of dice too.","`;dice <number of dice rolled (optional)>`","dices")
+  await ctx.send(embed=cmd.em)
+
+
+@client.command(aliases=["dices"])
+async def dice(ctx, num="1"):
+  try:
+    num=int(num)
+    for i in range(num):
+      roll = random.randint(1,6)
+      if roll == 1:
+        await ctx.send(file=discord.File('images/dice1.png'))
+      if roll == 2:
+        await ctx.send(file=discord.File('images/dice2.png'))
+      if roll==3:
+        await ctx.send(file=discord.File('images/dice3.png'))
+      if roll == 4:
+        await ctx.send(file=discord.File('images/dice4.png'))
+      if roll == 5:
+        await ctx.send(file=discord.File('images/dice5.png'))
+      if roll == 6:
+        await ctx.send(file=discord.File('images/dice5.png'))
+  except ValueError:
+    await ctx.send("Invalid Number!")
+
 keep_alive()
 # Run The Bot
 client.run(os.getenv("DISCORD_BOT_SECRET"))
